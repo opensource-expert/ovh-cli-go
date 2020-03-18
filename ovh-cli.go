84740d6db3db167802d5889a0f292e3b92c5f7a6
Sylvain Viart <sylvain303@github>
2020-03-18 08:10:52 +0100

    rewrite low level call API witout JSON decoding

--- ovh-cli.go
+++ ovh-cli.go
@@ -1,27 +1,66 @@
 package main
 
 import (
-	"encoding/json"
+	//"encoding/json"
 	"fmt"
+	// "github.com/davecgh/go-spew/spew"
+	"context"
 	"github.com/ovh/go-ovh/ovh"
+	"io/ioutil"
+	"os"
 )
 
 // Instantiate an OVH client and get the firstname of the currently logged-in user.
 // Visit https://api.ovh.com/createToken/index.cgi?GET=/me to get your credentials.
 func main() {
-	client, err := ovh.NewEndpointClient("ovh-eu")
+
+	// Initialize client, read credential in ovh.conf
+	// See documentation for locations: https://github.com/ovh/go-ovh#configuration
+	client, err := ovh.NewDefaultClient()
 	if err != nil {
-		fmt.Printf("Error: %q\n", err)
-		return
+		panic(err)
 	}
 
-	var result map[string]interface{}
-	client.Get("/me", &result)
-
-	str, err := json.Marshal(result)
+	// check input on stdin
+	fi, err := os.Stdin.Stat()
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
+	}
+
+	// mode of the stdin
+	fmt.Fprintf(os.Stderr, "%v\n", fi.Mode())
+
+	if fi.Size() > 0 {
+		fmt.Fprintf(os.Stderr, "there is something to read\n")
 	} else {
-		fmt.Println(string(str))
+		fmt.Fprintf(os.Stderr, "stdin is empty\n")
+	}
+
+	method := os.Args[1]
+	path := os.Args[2]
+
+	fmt.Fprintf(os.Stderr, "%s\n", path)
+
+	// call API, extracted from CallAPIWithContext()
+	ctx := context.Background()
+	req, err := client.NewRequest(method, path, nil, true)
+	if err != nil {
+		panic(err)
+	}
+	req = req.WithContext(ctx)
+	response, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	// Read all the response body
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		panic(err)
+	} else {
+		// output JSON
+		fmt.Println(string(body))
 	}
 }
